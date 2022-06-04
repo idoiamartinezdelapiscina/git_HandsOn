d06cad39cc40eae8240abf5714be4d1a40cba43d
idoiamartinezdelapiscina <mzidoia@hotmail.com>
2022-06-04 22:30:52 +0200

    new sentence

--- seqClass.py
+++ seqClass.py
@@ -19,4 +19,4 @@ if re.search('^[ACGTU]+$', args.seq):
     else:
         print ('The sequence can be DNA or RNA')
 else:
-    print ('The sequence is not DNA or RNA')
+    print ('weird sequence')
